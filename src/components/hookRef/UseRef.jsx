a4b7109d410f5e9d620f56b02b528e9a0ec26452
AnjasFedo <fedoaf98@gmail.com>
2024-02-05 12:31:17 +0700

    useRef to Access DOM

--- src/components/hookRef/UseRef.jsx
+++ src/components/hookRef/UseRef.jsx
@@ -1,33 +1,18 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 const UseRef = () => {
-  const [count, setCount] = useState(0);
+  const inputRef = useRef(null);
 
-  const countRef = useRef(0);
-
-  const increment = () => {
-    setCount((count) => count + 1);
-
-    countRef.current++;
-
-    console.log(count);
-    console.log(countRef)
-  };
-
-  const decrement = () => {
-    // setCount((count) => count - 1);
-
-    countRef.current--;
-    console.log(count);
-    console.log(countRef)
+  const focus = () => {
+    console.log(inputRef.current.className)
+    inputRef.current.focus();
   };
 
   return (
     <div>
-      <h1>{count}</h1>
-      <h1>{countRef.current}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <input type="text" className="anjas" ref={inputRef} />
+
+      <button onClick={focus}>fokus</button>
     </div>
   );
 };
